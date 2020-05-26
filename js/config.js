a943221672013f2555be0cbdd56585337cb1e983
Rulo Kobashikawa <akobashikawa@gmail.com>
2020-05-26 11:56:56 -0500

    Corrige url

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const config = {
   socketURL: "https://rtcmulticonnection.herokuapp.com:443/",
   stunServerURL: "stun:stun.l.google.com:19302",
-  turnServerURL: "numb.viagenie.ca",
+  turnServerURL: "turn:numb.viagenie.ca",
   turnServerUsername: "secret",
   turnServerPassword: "secret",
 };
