023d82515d9335701fd3f6bcb84bcab6600ff479
Rulo Kobashikawa <akobashikawa@gmail.com>
2020-05-31 16:42:33 -0500

    Comparto credenciales gratuitas

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ const config = {
   socketURL: "https://rtcmulticonnection.herokuapp.com:443/",
   stunServerURL: "stun:stun.l.google.com:19302",
   turnServerURL: "turn:numb.viagenie.ca",
-  turnServerUsername: "secret",
-  turnServerPassword: "secret",
+  turnServerUsername: "akobashikawa@gmail.com",
+  turnServerPassword: "*N70m0d4ch13",
 };
 export default config;
